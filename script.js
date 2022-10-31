4c7bb001c73a2cb65c2c3394476348bd3fcf0dad
anderson <anderson.ap133@gmail.com>
2022-10-31 16:22:33 -0300

    checkpoint

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 $(document).ready( function() {
+    var itemSelector = '.grid-item'; 
 
+	var $container = $('#container').isotope({
+		itemSelector: itemSelector,
+		masonry: {
+		  columnWidth: itemSelector,
+		  isFitWidth: true
+		}
+	});
 });
